2f9abe745ffd1f4e783366a534ba6ca15d5e8fc8
ars_qu <516605308@qq.com>
2019-11-26 16:38:24 +0800

    sms

---
+++ src/components/bookmark.vue
@@ -0,0 +1,21 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return { t: "test" };
+  },
+  created() {
+    this.a();
+  },
+  mounted() {},
+  methods: {
+    a() {
+      console.log("a");
+      console.log(this.t);
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped></style>
